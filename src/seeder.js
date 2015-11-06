82b965f3461a9cbf388181bedc03042d1bc03a84
holyshared <holy.shared.design@gmail.com>
2015-11-06 11:13:01 +0900

    add seeder source

--- src/seeder.js
+++ src/seeder.js
@@ -1,2 +1,45 @@
-export default function seeder(tableName, filePath) {
+import fs from 'fs';
+import parse from 'csv-parse';
+import iconv from 'iconv-lite';
+
+export default function seeder(tableName, filePath, encoding = 'utf8') {
+
+  return (knex, Promise) => {
+
+    return new Promise((resolve, reject) => {
+
+      let parser = parse({
+        delimiter: ',',
+        auto_parse: true
+      });
+
+      let queues = [
+        knex(tableName).del()
+      ];
+
+      let headers = [];
+
+      parser.on('readable', () => {
+        let obj = {};
+        let record = parser.read();
+
+        if (parser.count <= 1) {
+          headers = record;
+        } else {
+          headers.forEach((column, i) => { obj[column] = record[i]; });
+          queues.push( knex(tableName).insert(obj) );
+        }
+      });
+
+      parser.on('end', () => {
+        resolve( Promise.join.apply(Promise, queues) );
+      });
+
+      parser.on('error', reject);
+
+      let csv = fs.createReadStream(filePath);
+      csv.pipe( iconv.decodeStream(encoding) ).pipe(parser);
+    });
+  };
+
 }
