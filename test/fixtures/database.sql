db5518fa2585114699c1e7e93a01ebd7237dbca2
holyshared <holy.shared.design@gmail.com>
2015-12-17 14:02:22 +0900

    Remove default value from users schema

--- test/fixtures/database.sql
+++ test/fixtures/database.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
